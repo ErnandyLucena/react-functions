1e3c2080ba4c7609e9f2fcb8400071a6bff86f44
ErnandyLucena <ernandy2017@gmail.com>
2024-09-15 23:09:23 -0300

    commit2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../Components/Navbar'; // Importa o componente Navbar
 import Card from '../Components/Card';    // Importa o componente Card
+import Carousel from '../Components/Carousel';
 
 // Função principal do componente Home
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
       <Navbar />
       <div className="flex justify-center items-center min-h-screen">
         <Card />
+        <Carousel />
       </div>
     </div>
   );
